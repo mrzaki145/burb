f7a3242c1f8521f09326e5124d3fc123c9e8f440
Mostafa Reda <moszaki1234@gmail.com>
2022-10-03 13:41:03 +0200

    Bug fixes and code improvements

--- main.js
+++ main.js
@@ -1,114 +1,111 @@
-import Lenis from "@studio-freight/lenis";
+import gsap from "gsap";
 import Headroom from "headroom.js";
-import "matchmedia-polyfill/matchMedia";
-import "matchmedia-polyfill/matchMedia.addListener";
 
-import "./assets/styles/main.scss";
+document.addEventListener("DOMContentLoaded", () => {
+  const navEl = document.querySelector("#js_nav");
+  const navListEl = navEl.querySelector("#js_navList");
+  const navContentEl = navEl.querySelector("#js_navContent");
+  const toggleBtn = navEl.querySelector("#js_toggleBtn");
+  const navListAnime = gsap.from(navListEl, {
+    x: "100%",
+    duration: 0.3,
+    paused: true,
+  });
 
-const lenis = new Lenis({
-  lerp: 0.08,
-  smooth: true,
-});
+  new Headroom(navEl, {
+    classes: {
+      initial: "nav",
+      pinned: "nav--pinned",
+      unpinned: "nav--unpinned",
+      top: "nav--top",
+    },
+  }).init();
 
-function raf() {
-  lenis.raf();
-  requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
-
-// nav
-const navEl = document.querySelector("#js_nav");
-const navContentEl = navEl.querySelector("#js_navContent");
-const navListEl = navEl.querySelector("#js_navList");
-const toggleBtn = navEl.querySelector("#js_toggleBtn");
-
-const headroomOpts = {
-  classes: {
-    initial: "nav",
-    pinned: "nav--pinned",
-    unpinned: "nav--unpinned",
-    top: "nav--top",
-  },
-};
-const headroom = new Headroom(navEl, headroomOpts);
-headroom.init();
-
-toggleBtn.addEventListener("click", (e) => {
-  let isOpen = toggleBtn.getAttribute("aria-pressed");
-  isOpen = isOpen == "true";
-  toggleBtn.setAttribute("aria-pressed", !isOpen);
-  toggleBtn.setAttribute("aria-expanded", !isOpen);
-
-  if (!isOpen) {
-    lenis.stop();
-    navListEl.classList.add("open");
-    toggleBtn.querySelector("use").setAttribute("href", "#close");
-  } else {
-    lenis.start();
-    navListEl.classList.remove("open");
-    toggleBtn.querySelector("use").setAttribute("href", "#menu");
-  }
-});
-
-const mediaQuery = window.matchMedia("(max-width: 1023px)");
-function handleDeviceChange(e) {
-  if (e.matches) {
-    if (navListEl.closest("#js_navContent")) {
-      navEl.append(navListEl);
+  toggleBtn.addEventListener("click", () => {
+    if (
+      toggleBtn.getAttribute("aria-expanded") === "false" &&
+      navEl.dataset.listState == "closed"
+    ) {
+      navListAnime.play();
+      navEl.dataset.listState = "open";
+      toggleBtn.setAttribute("aria-expanded", "true");
+      toggleBtn.querySelector("use").setAttribute("href", "#close");
+    } else {
+      navListAnime.reverse();
+      navEl.dataset.listState = "closed";
+      toggleBtn.setAttribute("aria-expanded", "false");
+      toggleBtn.querySelector("use").setAttribute("href", "#menu");
     }
-  } else {
-    lenis.start();
-    if (!navListEl.closest("#js_navContent")) {
-      navContentEl.append(navListEl);
+  });
+
+  const mm = gsap.matchMedia();
+  mm.add(
+    { isMobile: "(max-width: 1023px)", isDesktop: "(min-width: 1024px)" },
+    (ctx) => {
+      const { isMobile, isDesktop } = ctx.conditions;
+
+      if (isMobile) {
+        navEl.append(navListEl);
+        // navListAnime.resetTo("x", "100%");
+      } else if (isDesktop) {
+        navEl.dataset.listState = "closed";
+        navContentEl.append(navListEl);
+        // navListAnime.resetTo("x", "0");
+      }
     }
-  }
-}
-mediaQuery.addListener(handleDeviceChange);
-mediaQuery.addEventListener("change", handleDeviceChange);
-handleDeviceChange(mediaQuery);
+  );
 
-// feedback
-const avatars = Array.from(document.querySelectorAll(".feedback .avatar"));
-const contents = Array.from(document.querySelectorAll(".feedback .content"));
+  // feedback
+  const avatars = Array.from(document.querySelectorAll(".feedback .avatar"));
+  const contents = Array.from(document.querySelectorAll(".feedback .content"));
 
-function switchFeedback() {
-  const activeAvatar = avatars.find((el) => el.classList.contains("active"));
-  const activeContent = contents.find((el) => el.classList.contains("active"));
-  const nextAvatar = activeAvatar.nextElementSibling
-    ? activeAvatar.nextElementSibling
-    : avatars[0];
-  const nextContent = activeContent.nextElementSibling
-    ? activeContent.nextElementSibling
-    : contents[0];
-
-  activeAvatar.classList.remove("active");
-  activeContent.classList.remove("active");
-  nextAvatar.classList.add("active");
-  nextContent.classList.add("active");
-}
-
-let interval = setInterval(switchFeedback, 10000);
-
-avatars.forEach((el) => {
-  el.addEventListener("click", (e) => {
-    const current = e.currentTarget;
+  function switchFeedback() {
     const activeAvatar = avatars.find((el) => el.classList.contains("active"));
-
-    if (current == activeAvatar) return;
-    clearInterval(interval);
-
     const activeContent = contents.find((el) =>
       el.classList.contains("active")
     );
+    const nextAvatar = activeAvatar.nextElementSibling
+      ? activeAvatar.nextElementSibling
+      : avatars[0];
     const nextContent = activeContent.nextElementSibling
       ? activeContent.nextElementSibling
       : contents[0];
 
     activeAvatar.classList.remove("active");
     activeContent.classList.remove("active");
-    current.classList.add("active");
+    nextAvatar.classList.add("active");
     nextContent.classList.add("active");
+  }
 
-    interval = setInterval(switchFeedback, 10000);
+  let interval = setInterval(switchFeedback, 10000);
+
+  avatars.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      const current = e.currentTarget;
+      const activeAvatar = avatars.find((el) =>
+        el.classList.contains("active")
+      );
+
+      if (current == activeAvatar) return;
+      clearInterval(interval);
+
+      const activeContent = contents.find((el) =>
+        el.classList.contains("active")
+      );
+      const nextContent = activeContent.nextElementSibling
+        ? activeContent.nextElementSibling
+        : contents[0];
+
+      activeAvatar.classList.remove("active");
+      activeContent.classList.remove("active");
+      current.classList.add("active");
+      nextContent.classList.add("active");
+
+      interval = setInterval(switchFeedback, 10000);
+    });
   });
 });
+
+window.onload = function () {
+  gsap.to(document.querySelector(".overlay"), { scaleY: 0 });
+};
